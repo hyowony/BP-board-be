3117abb02d2332614486fb39140d7aaaf728b383
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-14 03:25:51 +0900

    마무리

---
+++ for.js
@@ -0,0 +1,22 @@
+for (let i = 10000; i >= 1; i--) {
+  console.log(i);
+}
+
+// for (let i = 0; i < 101; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+
+// for (let i = 101; i > 0; i--) {
+//   if (i % 2 !== 0) console.log(i);
+// }
+
+///구구단 코드
+// for (let i = 1; i <= 9; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(j * i);
+//   }
+// }
+
+// for (let i = 1; i <= 9; i++) {
+//   console.log(i * 2);
+// }
---
+++ for1.js
@@ -0,0 +1,11 @@
+for (let i = 10000; i >= 1; i--) {
+  console.log(i);
+}
+
+// for (let i = 10000; i >= 1; i--) {
+//   if (i % 2 !== 0) console.log(i);
+// }
+
+// for (let i = 1; i <= 10000; i += 2) {
+//   console.log(i);
+// }
