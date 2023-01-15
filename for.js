a444cf1632575601d842cdb591a113995fe4658f
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-15 11:07:51 +0900

    포문 숙제 다듬기

--- for.js
+++ for.js
@@ -1,14 +1,20 @@
-for (let i = 10000; i >= 1; i--) {
-  console.log(i);
-}
+//10000부터 1까지 찍기
+
+// for (let i = 10000; i >= 1; i--) {
+//   console.log(i);
+// }
+
+//짝수만 찍기
 
 // for (let i = 0; i < 101; i++) {
 //   if (i % 2 === 0) console.log(i);
 // }
 
-// for (let i = 101; i > 0; i--) {
-//   if (i % 2 !== 0) console.log(i);
-// }
+//1,3,5,7,9 방식으로 홀수 찍기
+
+for (let i = 1; i <= 101; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
 
 ///구구단 코드
 // for (let i = 1; i <= 9; i++) {
