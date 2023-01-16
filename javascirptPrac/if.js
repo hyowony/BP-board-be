2b8ebc7e1b9da89a845b91d61a7da546efabad7c
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-16 14:45:20 +0900

    이프문 트라이캐치문 연습 추가

---
+++ javascirptPrac/if.js
@@ -0,0 +1,14 @@
+//IF, else문 연습
+
+//if는 조건문이라고 하기도 한다.
+
+const shoesPrice = 40000;
+
+if (shoesPrice < 50000) {
+  console.log("신발을 사겠습니다.");
+}
+
+const CapPrice = 50000;
+if (CapPrice <= 50000) {
+  console.log("모자를 사지 않겠습니다.");
+}
