2973d1f22d826a7a21c2e501d165df531d1d2ca1
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-25 20:59:34 +0900

    모듈까지는 완료

---
+++ javascirptPrac/fn.js
@@ -0,0 +1,24 @@
+///부가세 함수 익숙해지기 과제
+
+//화장실....
+
+
+
+const price = 10000 // 소비자가격
+const priceKrw = new Intl.NumberFormat('kr-KO', { style: 'currency', currency: 'KRW' }).format(price) + "원"
+let tax = price / 11 * 10
+tax = Math.round(tax)
+tax = new Intl.NumberFormat('kr-KO', { style: 'currency', currency: 'KRW' }).format(tax)
+tax = tax + "원"
+let originalPrice = price / 11
+originalPrice =  Math.round(originalPrice)
+originalPrice = new Intl.NumberFormat('kr-KO', { style: 'currency', currency: 'KRW' }).format(originalPrice)
+originalPrice = originalPrice + "원"
+
+console.log(price)
+
+console.log(priceKrw)
+
+console.log(tax)
+
+console.log(originalPrice)
